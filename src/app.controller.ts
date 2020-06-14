ab312c319a2e9233e2ba785cf60cf6f82c357282
Marc Stammerjohann <marcstammerjohann@web.de>
2020-06-14 15:24:51 +0200

    add swagger api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('prisma')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
